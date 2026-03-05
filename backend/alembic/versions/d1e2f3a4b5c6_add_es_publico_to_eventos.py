766ac26a4037bc73eeee4055584679ab4cc47fdf
M-formoso <tuteformoso@gmail.com>
2026-03-05 09:56:06 -0300

    Fix: corregir down_revision de migración es_publico

--- backend/alembic/versions/d1e2f3a4b5c6_add_es_publico_to_eventos.py
+++ backend/alembic/versions/d1e2f3a4b5c6_add_es_publico_to_eventos.py
@@ -1,7 +1,7 @@
 """add es_publico to eventos
 
 Revision ID: d1e2f3a4b5c6
-Revises: c81481935e3e
+Revises: 90ba6f7b2ad4
 Create Date: 2024-03-04 12:00:00.000000
 
 """
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = 'd1e2f3a4b5c6'
-down_revision: Union[str, None] = 'c81481935e3e'
+down_revision: Union[str, None] = '90ba6f7b2ad4'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
